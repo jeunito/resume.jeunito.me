6a0bba5eb27a2b23ec62527d95e0a51ef1acf843
Jose Asuncion <jeunito@gmail.com>
2019-04-24 22:29:01 -0700

    configure bucket to host static website

--- terraform/website.tf
+++ terraform/website.tf
@@ -1,4 +1,30 @@
 resource "aws_s3_bucket" "resume" {
   bucket = "${var.s3_bucket}"
-  acl = "private"
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "resume_bucket_policy" {
+  bucket = "${aws_s3_bucket.resume.id}"
+  policy = "${data.aws_iam_policy_document.resume_bucket_policy_document.json}"
+}
+
+data "aws_iam_policy_document" "resume_bucket_policy_document" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.resume.arn}/*"
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [ "*" ]
+    }
+  }
 }
